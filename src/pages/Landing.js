1a56bd1e8a975f686e81f6ef8efda19a8e11eebc
Muskan Jhamani <68344637+muskanjhamani@users.noreply.github.com>
2023-07-02 02:34:59 +0530

    Add files via upload

---
+++ src/pages/Landing.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import main from "../assets/images/main.svg";
+import Wrapper from "../assets/wrappers/LandingPage";
+import { Logo } from "../components";
+
+const Landing = () => {
+  return (
+    <Wrapper>
+      <nav>
+        <Logo />
+      </nav>
+      <div className="container page">
+        {/* info */}
+        <div className="info">
+          <h1>
+            job <span>tracking</span> app
+          </h1>
+          <p>
+            Looking for an entry-level job, a promotion or looking to gain
+            hands-on-experience in an entirely new industry.
+            Our website is a business-focused platform which enables its users to 
+            keep a track for their submitted job applications, upcoming interviews,
+            etc.,needed to reach out and make business connections. 
+          </p>
+          <Link to="/register" className="btn btn-hero">
+            Login / Register
+          </Link>
+        </div>
+        <img src={main} alt="job hunt" className="img main-img" />
+      </div>
+    </Wrapper>
+  );
+};
+
+export default Landing;
